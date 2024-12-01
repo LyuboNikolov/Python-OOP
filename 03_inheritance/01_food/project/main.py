1e69cd76b50675f766e15520e124098e99d3fbbb
LyuboNikolov <155641067+LyuboNikolov@users.noreply.github.com>
2024-12-01 14:05:15 +0100

    Add files via upload

---
+++ 03_inheritance/01_food/project/main.py
@@ -0,0 +1,6 @@
+from project.food import Food
+from project.fruit import Fruit
+
+food = Food("2024-12-01")
+fruit = Fruit("banana", "2024-12-02")
+print(fruit)
---
+++ 03_inheritance/04_multilevel_inheritance/project/main.py
@@ -0,0 +1,6 @@
+from project.sports_car import SportsCar
+
+sports_car = SportsCar()
+print(sports_car.move())
+print(sports_car.drive())
+print(sports_car.race())
